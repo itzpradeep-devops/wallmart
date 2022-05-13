8f4aa13d2870912bf8d0b73ee0c6405b40b097eb
itzpradeepp <itzpradeepdevops@gmail.com>
2022-05-14 03:05:12 +0530

    hintt

--- apple.rb
+++ apple.rb
@@ -1,3 +1,8 @@
 this is a ruby file . and i don't know about ruby files
+
 trying merge in remote repostry from STAGEBRANCH
-lets see
+
+
+
+cheryyyyyyyyyyyyyyyyyyy pickinnngggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg
+
